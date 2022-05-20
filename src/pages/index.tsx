8eeffacee408f691fda1b2180e024cd1b2f28008
felipe almeida batista <felipeab10@hotmail.com>
2022-05-20 16:44:39 -0400

    init creation of design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { GetStaticProps } from 'next';
-
+import { RichText } from 'prismic-dom';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
-
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
@@ -24,13 +26,62 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ posts }: Post) {
+  return (
+    <>
+      <main className={styles.container}>
+        {posts.map(post => (
+          <div key={post.uid} className={styles.post}>
+            <h1>{post.data.title}</h1>
+            <p>{post.data.subtitle}</p>
+            <div className={styles.footer}>
+              <span>
+                <FiCalendar />
+                {post.first_publication_date}
+              </span>{' '}
+              <span>
+                <FiUser />
+                {post.data.author}
+              </span>
+            </div>
+          </div>
+        ))}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+        <button type="button">Carregar mais posts</button>
+      </main>
+    </>
+  );
+}
 
-//   // TODO
-// };
+export const getStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType<any>('posts');
+  console.log(JSON.stringify(postsResponse, null, 2));
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+      data: {
+        title: RichText.asText(post.data.title),
+        subtitle: RichText.asText(post.data.subtitle),
+        author: RichText.asText(post.data.author),
+      },
+    };
+  });
+
+  console.log('posts', posts);
+
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
